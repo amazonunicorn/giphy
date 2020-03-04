87b70641ba258c8861453a64cfdb5561653c1eb5
EHatem03 <enhat6293@westinghousehs.org>
2020-03-04 23:31:16 +0000

    We are Bulletproof

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$("#search-button").click(function(){
-  
-  
-  
+$("#search-button").click(function () {
+        let custom_search_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC" ;
+    fetch(custom_search_url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            console.log(data);
+
+        })
+
 });
 
